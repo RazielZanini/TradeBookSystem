4a31e47b192e243b806001040d5cc631cbb99f2a
Raziel <raziel@za9.com.br>
2025-02-28 18:29:18 -0300

    first commit

---
+++ src/main/java/com/booktrader/BooktraderApplication.java
@@ -0,0 +1,13 @@
+package com.booktrader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooktraderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooktraderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/booktrader/BooktraderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.booktrader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooktraderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
