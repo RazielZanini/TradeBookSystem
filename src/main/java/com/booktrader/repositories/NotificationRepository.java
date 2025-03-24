c8217d24c11f1c5e02bf579ff7d280cd6910aef9
Raziel <raziel@za9.com.br>
2025-03-24 13:25:46 -0300

    fix: Ajustes para funcionalidade de serviço de notificações

--- src/main/java/com/booktrader/repositories/NotificationRepository.java
+++ src/main/java/com/booktrader/repositories/NotificationRepository.java
@@ -4,10 +4,9 @@ import com.booktrader.domain.notification.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
 
-    Optional<List<Notification>> findByUserIdAndReadFalse(Long userId);
+    List<Notification> findByUserIdAndReadFalse(Long userId);
 
 }
--- src/main/java/com/booktrader/services/NotificationService.java
+++ src/main/java/com/booktrader/services/NotificationService.java
@@ -1,14 +1,10 @@
 package com.booktrader.services;
 
-import com.booktrader.domain.book.Book;
 import com.booktrader.domain.notification.Notification;
-import com.booktrader.domain.user.User;
-import com.booktrader.dtos.NotificationDTO;
 import com.booktrader.repositories.NotificationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -24,8 +20,7 @@ public class NotificationService {
     public void saveNotification(Notification notification) { this.notificationRepository.save(notification); }
 
     public List<Notification> getUnreadNotifications(Long userId) throws Exception {
-        List<Notification> notifications = this.notificationRepository.findByUserIdAndReadFalse(userId)
-                .orElse(Collections.emptyList());
+        List<Notification> notifications = this.notificationRepository.findByUserIdAndReadFalse(userId);
 
         if (notifications.isEmpty()) {
             throw new Exception("Nenhuma notificação encontrada.");
@@ -38,17 +33,4 @@ public class NotificationService {
         return notifications;
     }
 
-
-    public Notification createNotification(NotificationDTO data) throws Exception{
-
-        User user = this.userService.findUserById(data.userId());
-        Book userBook = this.bookService.findBookById(data.userBookId());
-        Book tradeBook = this.bookService.findBookById(data.tradeBookId());
-
-        Notification newNotification = new Notification(data.message(), userBook, tradeBook, user, data.tradeId());
-
-        this.saveNotification(newNotification);
-
-        return newNotification;
-    }
 }
