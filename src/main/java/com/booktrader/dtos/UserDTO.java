ff219ff8a1e87f13d3da69dfe5973b3253f0dbad
Raziel <raziel@za9.com.br>
2025-04-30 20:01:49 -0300

    Feat: Adicionada função de conversão de UserDTO para User

--- src/main/java/com/booktrader/dtos/UserDTO.java
+++ src/main/java/com/booktrader/dtos/UserDTO.java
@@ -1,6 +1,11 @@
 package com.booktrader.dtos;
 
+import com.booktrader.domain.user.User;
 import com.booktrader.domain.user.UserRole;
 
 public record UserDTO(String name, String password, UserRole role, String email) {
+
+    public User toUser(UserDTO userDTO){
+        return new User(userDTO.name(), userDTO.password(), userDTO.role(), userDTO.email());
+    }
 }
