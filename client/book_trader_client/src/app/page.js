81f89ffaad98455ef5052457c09c4ff74b107a2b
Raziel <raziel@za9.com.br>
2025-05-13 15:38:05 -0300

    Feat: Alterações no layout e mudanã de page padrão para tela home

--- client/book_trader_client/src/app/page.js
+++ client/book_trader_client/src/app/page.js
@@ -1,69 +1,40 @@
 'use client'
-
-import api from "@/utils/api";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
 
-export default function Register() {
+export default function Home() {
 
-  const [user, setUser] = useState({});
-  const [localData, setLocalData] = useState({})
-  const route = useRouter();
-
-  useEffect(() => {
-
-    if (localData) {
-      localStorage.setItem('user', JSON.stringify(localData))
-    }
-
-  }, [localData])
-
-  async function createUser() {
-
-    if (!user.password || !user.name) {
-      alert('Preencha as informações para efetuar o cadastro!')
-      return
-    }
-
-    try {
-      const result = await api.post('/users', user)
-      console.log(result.data)
-      alert('Conta criada com sucesso')
-      setLocalData(result.data)
-      route.push('/home')
-    } catch (error) {
-      alert('Erro ao criar conta')
-      console.log(error)
-    }
-  }
+  const router = useRouter();
 
   return (
-    <div className="w-full min-h-screen flex flex-col items-center justify-center">
-      <div className="border border-white w-6/12 max-w-lg bg-slate-200 text-black rounded-md shadow-2xl p-6 flex flex-col items-center justify-center gap-4">
-        <h1 className="font-medium text-2xl">Cadastre-se</h1>
-        <div className="flex flex-col gap-2">
-          <label htmlFor="input">Usuário</label>
-          <input className="border-2 border-slate-400 rounded-md p-2"
-            type="text"
-            placeholder="Digite seu usuário"
-            onChange={(e) => setUser({ ...user, name: e.target.value })}
-            value={user.name || ''}
+    <>
+      <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-200 p-4">
+        <header className="mb-10 text-center">
+          <h1 className="text-5xl font-bold text-indigo-900">TradeBookSystem</h1>
+          <p className="text-lg text-indigo-700 mt-2">Seu app de troca de livros</p>
+        </header>
+
+        <div className="bg-white rounded-2xl shadow-xl p-8 max-w-md w-full text-center">
+          <img src="https://cdn-icons-png.flaticon.com/512/29/29302.png"
+            alt="Logo de livro"
+            className="w-24 h-24 mx-auto mb-4"
           />
-          <label htmlFor="input">Senha</label>
-          <input className="border-2 border-slate-400 rounded-md p-2"
-            type="password"
-            placeholder="Digite sua senha"
-            onChange={(e) => setUser({ ...user, password: e.target.value })}
-            value={user.password || ''}
-          />
-        </div>
-        <div>
-          <button className="bg-emerald-400 p-2 rounded-md hover:bg-emerald-500 cursor-pointer w-60"
-            onClick={() => createUser()}
-          >Cadastrar
-          </button>
+
+          <h2 className="text-2xl font-semibold mb-2 text-gray-800">Bem vindo ao TradeBookSystem!</h2>
+          <p className="text-gray-600 mb-6">
+            Troque livro com outrsas pessoas, descubra novas histórias e compartilhe suas reviews com todo os usuários!
+          </p>
+          <div className="flex justify-center gap-4">
+            <button className="bg-indigo-600 text-white px-6 py-2 rounded-xl hover:bg-indigo-700 transition"
+              onClick={() => router.push('/login')}>
+              Entrar
+            </button>
+            <button className="bg-gray-200 text-indigo-700 px-6 py-2 rounded-xl hover:bg-gray-300 transition"
+              onClick={() => router.push('/register')}>
+              Cadastrar
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
