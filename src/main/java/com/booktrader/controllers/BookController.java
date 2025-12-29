cb119768dc35f2702d3e517b5d918f8c6340bc4a
Raziel <raziel@za9.com.br>
2025-12-28 21:17:29 -0300

    clran code

--- src/main/java/com/booktrader/controllers/BookController.java
+++ src/main/java/com/booktrader/controllers/BookController.java
@@ -1,15 +1,11 @@
 package com.booktrader.controllers;
 
-import com.booktrader.domain.book.Book;
-import com.booktrader.domain.review.Review;
 import com.booktrader.dtos.SuccessMessageDTO;
 import com.booktrader.dtos.request.RequestBookDTO;
 import com.booktrader.dtos.response.ResponseBookDTO;
 import com.booktrader.dtos.response.ResponseReviewDTO;
 import com.booktrader.services.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +20,7 @@ public class BookController {
     private BookService bookService;
 
     @PostMapping
-    public ResponseEntity<ResponseBookDTO> createBook(@RequestBody RequestBookDTO book) throws Exception{
+    public ResponseEntity<ResponseBookDTO> createBook(@RequestBody RequestBookDTO book){
         ResponseBookDTO newBook = this.bookService.createBook(book);
         return new ResponseEntity<>(newBook, HttpStatus.CREATED);
     }
@@ -36,19 +32,19 @@ public class BookController {
     }
 
     @PutMapping("/{bookId}")
-    public ResponseEntity<ResponseBookDTO> editBook(@RequestBody RequestBookDTO data, @PathVariable("bookId") Long bookId) throws Exception {
+    public ResponseEntity<ResponseBookDTO> editBook(@RequestBody RequestBookDTO data, @PathVariable("bookId") Long bookId){
         ResponseBookDTO updatedBook = this.bookService.updateBook(bookId, data);
         return new ResponseEntity<>(updatedBook, HttpStatus.CREATED);
     }
 
     @DeleteMapping("/{bookId}")
-    public ResponseEntity<SuccessMessageDTO> deleteBook(@PathVariable("bookId") Long bookId) throws Exception{
+    public ResponseEntity<SuccessMessageDTO> deleteBook(@PathVariable("bookId") Long bookId){
         SuccessMessageDTO message = this.bookService.deleteBook(bookId);
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 
     @GetMapping("/{bookId}/reviews")
-    public ResponseEntity<List<ResponseReviewDTO>> getBookReviews(@PathVariable Long bookId) throws Exception{
+    public ResponseEntity<List<ResponseReviewDTO>> getBookReviews(@PathVariable Long bookId){
         List<ResponseReviewDTO> reviews = this.bookService.getBookReviews(bookId);
         return new ResponseEntity<>(reviews, HttpStatus.OK);
     }
